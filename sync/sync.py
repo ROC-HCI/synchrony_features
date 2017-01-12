f190aa58780089ebd94bea20c9ec623f6ce9b63d
taylanksen <tsen@cs.rochester.edu>
2017-01-12 11:07:35 -0500

    code cleanup, added skeleton for yu_sync alg.

--- sync/sync.py
+++ sync/sync.py
@@ -29,98 +29,120 @@ If you want to set the logging level from a command-line option such as:
 #------------------
 class Sync:
     """ Synchrony calculation  
-    
+        Class for storing raw data and synchrony based results. Contains methods 
+        for multiple algorithms.
     
     
     """
     
+    #-------------------------------
     def __init__(self, X_, Y_):
         self.SAMPLE_RATE = 15
         self.X = np.array(X_)
         self.Y = np.array(Y_)
         
-        assert (len(X_) == len(Y_)), 'array sizes are different'
+        assert (len(X_) == len(Y_)), 'X and Y array sizes are different'
         
-        self.X_thresh = []
-        self.Y_thresh = []
+        self.X_quant = []
+        self.Y_quant = []
  
-        self.THRESH = 50.
-    
-    def calculate_sync(self, max_time_shift_=37):
-
+        self.THRESH = 0
         
-        # threshold X and Y
-        #self.X_thresh = (X_ > THRESH_).astype(int)
-        #self.Y_thresh = (Y_ > THRESH_).astype(int)
-
-        self.X_thresh = np.zeros_like(self.X, float)
+    #-------------------------------
+    def quantize_data(self, THRESH_):
+        """ for X and Y, determines whether each value is > THRESH, 
+            and puts result into self.X_quant and self.Y_quant
+        """
+        self.THRESH = THRESH_
+        
+        self.X_quant = np.zeros_like(self.X, float)
         for t, x in enumerate(self.X):
             if np.isnan(x): 
-                self.X_thresh[t] = np.nan
+                self.X_quant[t] = np.nan
             else:
-                self.X_thresh[t] = x > self.THRESH
+                self.X_quant[t] = x > self.THRESH
                 
         
-        self.Y_thresh = np.zeros_like(self.Y, float)
+        self.Y_quant = np.zeros_like(self.Y, float)
         for t, y in enumerate(self.Y):
             if np.isnan(y): 
-                self.Y_thresh[t] = np.nan
+                self.Y_quant[t] = np.nan
             else:
-                self.Y_thresh[t] = y > self.THRESH
+                self.Y_quant[t] = y > self.THRESH
+       
+    #-------------------------------
+    def calculate_corr_sync(self, slot_size_=22, max_time_shift_=37):
+        """ Splits X into slots. For each X slot, calculates the correlation
+            with every Y slots following the X slot time up to max_time_shift.
+            -------------------------------
+            return value: 
+               x2y_sync, y2x_sync            
+        """
+        pass
+    
+    #-------------------------------    
+    def calc_edge_trig_sync(self, THRESH_, max_time_shift_=37):
+        """ given raw data stored in X and Y, calculates synchrony using
+            the "edge triggered algorithm.  In turn treats X as the sender
+            and Y as the receiver, then Y as the sender, X as the receiver.
+            For each sender rising edge, checks if there is any positive 
+            in Y in the max_time_shift following the time at X rising edge 
+            --------------------------
+            return value: 
+               x2y_sync, y2x_sync
+        """
+        
+        self.quantize_data(THRESH_)
                 
         # find rising edges in X
-        x_rise_cnt = 0 # the number of rising edges in X_thresh        
         x_rising_edges = [] # list of times where there is a rising edge
         x_last = 0
-        for t, x in enumerate (self.X_thresh):
+        for t, x in enumerate (self.X_quant):
             # is it a rising edge
             if ( (x_last == 0) and (x == 1) ):
-                x_rise_cnt += 1
                 x_rising_edges.append(t)
  
                 logging.debug('rising edge at t :' + str(t))
-                logging.debug('rising edge cnt:' + str(x_rise_cnt))
+                logging.debug('rising edge cnt:' + str(len(x_rising_edges)))
             x_last = x
             
         # find rising edges in Y
-        y_rise_cnt = 0
         y_rising_edges = []
         y_last = 0
-        for t, y in enumerate (self.Y_thresh):
+        for t, y in enumerate (self.Y_quant):
             # is it a rising edge
-            if ( (y_last == 0) and (y == 1) ):
-                y_rise_cnt += 1
-                
+            if ( (y_last == 0) and (y == 1) ):                
                 y_rising_edges.append(t)
  
                 logging.debug('y rising edge at t :' + str(t))
-                logging.debug('y rising edge cnt:' + str(y_rise_cnt))
+                logging.debug('y rising edge cnt:' + str(len(y_rising_edges)))
             y_last = y
             
         # count y follows x
-        shared_cnt = 0
+        x2y_shared_cnt = 0
         for t in x_rising_edges:
-            window = self.Y_thresh[t:t+max_time_shift_]
+            window = self.Y_quant[t:t+max_time_shift_]
             window_is_one = (window == 1) # just to make sure that nan does not give positive
             if np.any(window_is_one):
-                shared_cnt += 1
+                x2y_shared_cnt += 1
 
-        y_sync = float(shared_cnt) / x_rise_cnt
+        x2y_sync = float(x2y_shared_cnt) / len(x_rising_edges)
 
 
         # count x follows y
-        shared_cnt = 0
+        y2x_shared_cnt = 0
         for t in y_rising_edges:
-            window = self.X_thresh[t:t+max_time_shift_]
+            window = self.X_quant[t:t+max_time_shift_]
             window_is_one = (window == 1) # just to make sure that nan does not give positive
             if np.any(window_is_one):
-                shared_cnt += 1
+                y2x_shared_cnt += 1
 
-        x_sync = float(shared_cnt) / y_rise_cnt
+        y2x_sync = float(y2x_shared_cnt) / len(y_rising_edges)
 
         
-        return x_sync, y_sync
+        return x2y_sync, y2x_sync
 
+    #-------------------------------
     def plot(self):
         # Sample rate and desired cutoff frequencies (in Hz).
         plt.figure(1)
@@ -132,35 +154,37 @@ class Sync:
         plt.plot(time, self.X)
         plt.xlabel('Time (sec)')
         plt.title('X')
-        plt.axhline(y=self.THRESH)
+        plt.axhline(y=self.THRESH, color='r')
         plt.grid()
 
         plt.subplot(222)
-        plt.plot(time, self.X_thresh)
-        plt.fill_between(time, 0, self.X_thresh, where=self.X_thresh >= 0, facecolor='green', interpolate=True)
-        plt.title('X_thresh')
+        plt.plot(time, self.X_quant)
+        plt.fill_between(time, 0, self.X_quant, where=self.X_quant >= 0, 
+                         facecolor='green', interpolate=True)
+        plt.title('X_quant')
         plt.grid()
 
         plt.subplot(223)
         plt.plot(time, self.Y)
         plt.xlabel('Time (sec)')
         plt.title('Y')
-        plt.axhline(y=self.THRESH)
+        plt.axhline(y=self.THRESH, color='r')
         plt.grid()
 
         plt.subplot(224)
-        plt.plot(time, self.Y_thresh)
-        plt.fill_between(time, 0, self.Y_thresh, where=self.Y_thresh >= 0, facecolor='green', interpolate=True)
-        plt.title('Y_thresh')
+        plt.plot(time, self.Y_quant)
+        plt.fill_between(time, 0, self.Y_quant, where=self.Y_quant >= 0, 
+                         facecolor='green', interpolate=True)
+        plt.title('Y_quant')
         plt.grid()
         
         plt.tight_layout()
         plt.show()      
 
-
+    #-------------------------------
     def __str__(self):
+        # TODO
         s = "Syncrony: "
-        s += str(self.synchrony)
         return s
 
 #============================================================================
@@ -193,8 +217,8 @@ class TestSync(unittest.TestCase):
         
         my_sync = Sync(X, Y)
 
-        my_sync.THRESH = 5
-        x_sync, y_sync = my_sync.calculate_sync()
+        x2y_sync, y2x_sync = my_sync.calc_edge_trig_sync(THRESH_=50, 
+                                                     max_time_shift_=37)
         my_sync.plot()
         print(my_sync)
         
@@ -224,7 +248,9 @@ class TestSync(unittest.TestCase):
         my_sync = Sync(X[:smaller_N],Y[:smaller_N])
         my_sync.THRESH = 5
         
-        synchrony = my_sync.calculate_sync()
+        x2y_sync, y2x_sync = my_sync.calc_edge_trig_sync(THRESH_=50, 
+                                                         max_time_shift_=37)
+
         my_sync.plot()
         print(my_sync)
         
@@ -283,4 +309,5 @@ def do_all():
 #=============================================================================
 if __name__ == '__main__':
     print('running main')
+    unittest.main()
     do_all()
